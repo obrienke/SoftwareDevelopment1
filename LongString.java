668b72a0e9e1c5bfb16605fc81d30c704ed53589
obrienke <kevin.obrien@gmit.ie>
2021-10-18 15:40:18 +0000

    week 4 lecture - sample code

---
+++ LongString.java
@@ -0,0 +1,25 @@
+public class LongString{
+    public static void main(String[] args){
+        System.out.println("The original novel told the story of the friendship between a large male bear" + 
+                            "named Ben and a boy named Mark. The story provided the basis for the 1967 film" +
+                            " Gentle Giant (1967), the popular late 1960s U.S. television series Gentle Ben," + 
+                            " a 1970s animated cartoon and two early 2000s made-for-TV movies.");
+
+        System.out.println("Kevin O'Brien");
+        System.out.println("Kevin" + " O'Brien");
+        System.out.println("Mr. " + "Kevin" + " O'Brien");
+        System.out.println("Mr. " + "Kevin" + " O'Brien" + " I");
+
+        //Variables
+        String firstname = "Kevin";
+        firstname = "John";
+        String lastname = "Smith";
+
+        System.out.println("Name: " + firstname + " " + lastname);
+
+        int number = 9;
+
+
+
+    }
+}
