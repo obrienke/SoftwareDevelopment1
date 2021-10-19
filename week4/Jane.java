689bdb526df7abf7b4db2ae7bf5425c5ceb16393
obrienke <kevin.obrien@gmit.ie>
2021-10-19 14:45:18 +0000

    exercise 6

---
+++ week4/Jane.java
@@ -0,0 +1,13 @@
+public class Jane {
+    //Jane is 20 years old and is studying English in NUIG. Some day she hopes to be a journalist
+    public static void main(String[] args){
+        String name = "Jane";
+        int age = 20;
+        String subject = "English";
+        String college = "NUIG";
+        String career = "journalist";
+
+        System.out.println(name+" is "+age+" years old and is studying "+subject+" in "+college+".");
+        System.out.println("Some day she hopes to be a "+career);
+    }
+}
