dc3d29d8107121c6041fe5f8caba2d190ae9b174
obrienke <kevin.obrien@gmit.ie>
2021-10-29 14:39:22 +0100

    Update Addition.java

--- week6/Addition.java
+++ week6/Addition.java
@@ -6,15 +6,15 @@ public class Addition{
       Scanner input = new Scanner(System.in);
 
       int number1; // first number input by user
-	  int number2; // second number input by user
-	  int sum; //variable to store result
+      int number2; // second number input by user
+      int sum; //variable to store result
 
       System.out.print("Enter first integer: "); // prompt for input
       x = input.nextInt(); // read first integer
       System.out.print("Enter second integer: "); // prompt for input
       y = input.nextInt(); // read first integer
 
-	  sum = number1 + number2;
+      sum = number1 + number2;
       System.out.println(number1 + " + " + number2 + " = " +sum);
    } // end method main
 } // end class Addition
