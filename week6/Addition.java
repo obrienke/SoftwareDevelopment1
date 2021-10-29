fa2500282428c246f34b7436c65ce8efef41356f
obrienke <kevin.obrien@gmit.ie>
2021-10-29 14:38:44 +0100

    Java From Lecture Slides

---
+++ week6/Addition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // program uses Scanner
+
+public class Addition{
+   public static void main(String[] args){
+      // create Scanner to obtain input from command window
+      Scanner input = new Scanner(System.in);
+
+      int number1; // first number input by user
+	  int number2; // second number input by user
+	  int sum; //variable to store result
+
+      System.out.print("Enter first integer: "); // prompt for input
+      x = input.nextInt(); // read first integer
+      System.out.print("Enter second integer: "); // prompt for input
+      y = input.nextInt(); // read first integer
+
+	  sum = number1 + number2;
+      System.out.println(number1 + " + " + number2 + " = " +sum);
+   } // end method main
+} // end class Addition
+
