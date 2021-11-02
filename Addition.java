d3ea550fe505a79350e90ee958aeed2b5942d4f4
obrienke <kevin.obrien@gmit.ie>
2021-11-02 15:03:58 +0000

    updated errors

---
+++ Addition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // program uses Scanner
+
+public class Addition{
+   public static void main(String[] args){
+      // create Scanner to obtain input from command window
+      Scanner input = new Scanner(System.in);
+
+      int number1; // first number input by user
+      int number2; // second number input by user
+      int sum; //variable to store result
+
+      System.out.print("Enter first integer: "); // prompt for input
+      number1 = input.nextInt(); // read first integer
+      System.out.print("Enter second integer: "); // prompt for input
+      number2 = input.nextInt(); // read first integer
+
+      sum = number1 + number2;
+      System.out.println(number1 + " + " + number2 + " = " +sum);
+      input.close();
+   } // end method main
+} // end class Addition
