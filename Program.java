da25d251d60d9f62cdf1a7196afb6f8e31b3c26e
obrienke <kevin.obrien@gmit.ie>
2021-11-15 15:42:48 +0000

    Program Template for Lab Exercises

---
+++ Program.java
@@ -0,0 +1,24 @@
+/* 
+Program Template which can be used for Lab Exercises
+*/
+
+//uncomment next line if inputs are required in your program
+//import java.util.Scanner;
+
+public class Program{
+	public static void main(String[] args){
+		//uncomment next line if inputs are required in your program
+		//Scanner input = new Scanner(System.in);
+		
+		//Declare and initialise program variables
+		
+		//Include prompts for inputs and assign inputs to variables
+		
+		//Other logic - arithmetic and decision making logic
+		
+		//Outputs
+		
+		//uncomment next line if inputs are required in your program
+		//input.close();	
+	}
+}
