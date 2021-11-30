a371f5b9abaf5da4a20d39062e45ae79fa642e11
obrienke <kevin.obrien@gmit.ie>
2021-11-30 16:39:44 +0000

    Week 10 Lab Solutions

--- week10/Question1.java
+++ week10/Question1.java
@@ -64,9 +64,90 @@ public class Question1{
 		}else{
 			System.out.println("incorrect");
 		}
-		
+
+		System.out.println("Question 3");
+		System.out.println("The code below produces which output to the screen?");
 		System.out.println();
-		System.out.println("You scored "+total+" out of 2");
+		System.out.println("int x = 1;");
+		System.out.println();
+		System.out.println("if( x != 1){");
+		System.out.println("   System.out.println(\"not equal to 1\");");
+		System.out.println("}else{");
+		System.out.println("   System.out.println(\"equal to 1\");");
+		System.out.println("}");
+		System.out.println();
+		System.out.println("Select option");
+		System.out.println("1. not equal to 1");
+		System.out.println("2. equal to 1");
+		System.out.println("3. no output");
+		System.out.println("4. error");
+		System.out.println();
+
+		System.out.print("Enter choice (1, 2, 3 or 4): ");
+		choice = input.nextInt();
+
+		switch(choice){
+			case 1: 
+				System.out.println("incorrect");
+				break;
+			case 2:
+				System.out.println("correct");
+				total = total + 1;
+				break;				
+			case 3:
+				System.out.println("incorrect");
+				break;
+			case 4:
+				System.out.println("incorrect");
+				break;
+			default:
+				System.out.println("invalid entry");
+		}
+
+		System.out.println("Question 4");
+		System.out.println("String num = \"22\"");
+		System.out.println("What is the value of num. Do not include quotation marks");
+		System.out.println();
+		String answer = input.next();
+		if(answer.equals("22")){
+			System.out.println("correct");
+			total = total + 1;
+		}else{
+			System.out.println("incorrect");
+		}
+
+		System.out.println("Question 5");
+		System.out.println("How many choices are possible when using a single if-else statement?");
+		System.out.println();
+		System.out.println("Select option");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println();
+
+		System.out.print("Enter choice (1, 2, 3 or 4): ");
+		choice = input.nextInt();
+
+		if(choice == 2){
+			System.out.println("correct");
+			total = total + 1;
+		}else{
+			System.out.println("incorrect");
+		}		
+		System.out.println();
+		System.out.println("You scored "+total+" out of 5");
+		double result = total * 1.0 / 5 * 100;
+		System.out.println("Score as a percentage: "+result);
+		
+		if(result >= 80){
+			System.out.println("Distinction");
+		}else if(result >= 40){
+			System.out.println("Pass");
+		}else{
+			System.out.println("Fail");
+		}
+
 		
 		//Other logic - arithmetic and decision making logic
 		
