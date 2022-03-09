b7ca6384dc9831e0d90cb3b9906d3f6ff1261fa3
obrienke <kevin.obrien@gmit.ie>
2022-03-09 22:05:33 +0000

    Sample Assessment Solutions

---
+++ week19/Exercise4.java
@@ -0,0 +1,11 @@
+public class Exercise4{
+    public static void main(String[] args) {
+        for(int rows = 1; rows <= 6; rows++){
+            System.out.print("row " + rows + ":");
+            for(int columns = 1; columns <= 4; columns++){
+                System.out.print(" column " + columns);
+            }
+            System.out.println();
+        }
+    }
+}
