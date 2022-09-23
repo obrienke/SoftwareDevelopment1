5549b83c66301844efd088a69ec0a7e75d405adc
obrienke <kevin.obrien@gmit.ie>
2022-09-23 14:55:15 +0100

    week 1 code

--- week1/Welcome2.java
+++ week1/Welcome2.java
@@ -1,6 +1,14 @@
+// Welcome2.java
+// Text-printing program
+
 public class Welcome2{
-    public static void main(String[] args) {
+
+    //main method begins the execution of Java Application
+    public static void main(String[] args){
+  
         System.out.print("Welcome to");
         System.out.println(" Java Programming!");
-    }
-}
+  
+    }//end of main method
+  
+}//end of class
--- week1/Welcome3.java
+++ week1/Welcome3.java
@@ -1,5 +1,13 @@
+// Welcome3.java
+// Text-printing program
+
 public class Welcome3{
-    public static void main(String[] args) {
+
+    //main method begins the execution of Java Application
+    public static void main(String[] args){
+  
         System.out.println("Welcome\nto\nJava\nProgramming!");
-    }
-}
+  
+    }//end of main method
+  
+}//end of class
