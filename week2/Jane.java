b046bdc8ed649be8f3eaf5c2d3f57265863e054d
obrienke <kevin.obrien@gmit.ie>
2022-10-14 11:44:33 +0000

    week 2 lab

---
+++ week2/Jane.java
@@ -0,0 +1,14 @@
+public class Jane{
+  public static void main(String[] args) {
+    String name = "Jane";
+    int age = 20;
+    String subject = "English";
+    String college = "ATU";
+    String job = "Journalist";
+
+    System.out.println(name + " is " + age + " years old." +
+                      "She is studying " + subject + " " + subject +
+                      " in " + college + ".");
+    System.out.println("Some day she hopes to be a " + job + ".");
+  }
+}
