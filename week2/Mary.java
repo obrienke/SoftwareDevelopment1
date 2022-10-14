28eaa82524c6fe882db4e017414839a4cf384d2c
obrienke <kevin.obrien@gmit.ie>
2022-10-14 11:28:09 +0000

    week 2 lab

---
+++ week2/Mary.java
@@ -0,0 +1,10 @@
+public class Mary{
+  public static void main(String[] args) {
+    String name = "Mary";
+    int age = 20;
+    String hobby = "running";
+
+    System.out.println(name + " is " + age + " years old." +
+                      "Her favourite hobby is " + hobby + ".");
+  }
+}
