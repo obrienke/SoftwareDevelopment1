735fab0027b69b6217f294ce7919f0c6cc3e5c7c
obrienke <kevin.obrien@gmit.ie>
2022-10-24 10:28:17 +0000

    week 5 code examples

--- week5/Addition.java
+++ week5/Addition.java
@@ -6,15 +6,15 @@ public class Addition{
      Scanner input = new Scanner(System.in);
 
      int number1; // first number input by user
-	  int number2; // second number input by user
-	  int sum; //variable to store result
+     int number2; // second number input by user
+     int sum; //variable to store result
 
      System.out.print("Enter first integer: "); // prompt for input
      number1 = input.nextInt(); // read first integer
      System.out.print("Enter second integer: "); // prompt for input
      number2 = input.nextInt(); // read first integer
 
-	  sum = number1 + number2;
+     sum = number1 + number2;
      System.out.println(number1 + " + " + number2 + " = " +sum);
      input.close();
    } // end method main
