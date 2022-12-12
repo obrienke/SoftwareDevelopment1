9e31a6d1f36876eed0f06133007f4354693ee548
obrienke <kevin.obrien@gmit.ie>
2022-12-12 10:12:22 +0000

    revision lab exercise solutions

---
+++ week11/W.java
@@ -0,0 +1,12 @@
+public class W{
+  public static void main(String[] args){
+    System.out.println();
+    System.out.println(" ***        ****        ***");
+    System.out.println("  ***      ******      ***");
+    System.out.println("   ***    ***  ***    ***");
+    System.out.println("    ***  ***    ***  ***");
+    System.out.println("     ******      ******");
+    System.out.println("      ****        ****");
+    System.out.println();
+  }
+}
