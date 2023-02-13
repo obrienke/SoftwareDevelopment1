0af017f0fbdaf11e140ccec17e52514e10c3732d
obrienke <kevin.obrien@gmit.ie>
2023-02-13 10:25:07 +0000

    For Loop Example from Lecture

---
+++ week16/Lecture.java
@@ -0,0 +1,13 @@
+public class Lecture{
+    public static void main(String[] args) {
+        int x = 1;
+        while(x <= 5){
+            System.out.println(x + ". While Loop");
+            x++;
+        }
+        System.out.println();
+        for(int y = 1; y <= 5; y++){
+            System.out.println(y + ". For Loop");
+        }
+    }
+}
