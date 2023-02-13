31ca7ec730cb1b36d7e539558b2bd3eb873b22a0
obrienke <kevin.obrien@gmit.ie>
2023-02-13 10:28:10 +0000

    Solutions to some week 15 lab exercises

---
+++ week15/TwentyNums.java
@@ -0,0 +1,22 @@
+public class TwentyNums{
+   public static void main(String[] args){
+	 int i = 1;
+	 while (i <= 20) {
+	    System.out.print(i + " ");
+	    i++;
+      }
+
+	 System.out.println();
+
+     i = 0;
+	 while (i <= 20) {
+	    System.out.print(i);
+	    if (i < 20){
+		  System.out.print(" + ");
+	    }
+	    i++;
+      }
+      System.out.println();
+
+	} // end main
+} // end class
