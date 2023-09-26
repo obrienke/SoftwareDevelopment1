1d2f4f94eaad211b1b97983a0f0ee69dbd8fde1b
obrienke <kevin.obrien@gmit.ie>
2023-09-26 13:51:12 +0000

    Code from Lecture Week 2

---
+++ week2/Lecture.java
@@ -0,0 +1,13 @@
+public class Lecture{
+    public static void main(String[] args) {
+
+        int x = 5;
+        
+        System.out.println("Number is " + x);
+
+        x = 10;
+        
+        System.out.println("Number is updated to " + x);
+        
+    }
+}
