ba8522618410d53e07a43a2e31e3dff9947cba0c
obrienke <kevin.obrien@gmit.ie>
2023-10-02 09:45:13 +0000

    week 3 lab code

---
+++ week3/GentleBen.java
@@ -0,0 +1,9 @@
+public class GentleBen{
+    public static void main(String[] args) {
+        System.out.println("The original novel told the story of the friendship between a large male bear" +
+                            " named Ben and a boy named Mark. The story provided the basis for the 1967" +
+                            " film Gentle Giant (1967), the popular late 1960s U.S. telvision series" +
+                            " Gentle Ben, a 1980s animated cartoon and two early 2000s made-for-TV movies."
+        );
+    }
+}
---
+++ week3/StringConcat.java
@@ -0,0 +1,9 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        int age = 20;
+
+        System.out.println("Age: " + age);
+        System.out.println(age + " years old.");
+        System.out.println("Age is " + age + " years old.");
+    }
+}
