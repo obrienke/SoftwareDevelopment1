e26a03bc4e9417f8fb6f15d4737c6d67078a7bac
obrienke <kevin.obrien@gmit.ie>
2023-10-03 08:17:08 +0000

    exercises 1 to 4 from lab

---
+++ week3/PrimTypes.java
@@ -0,0 +1,31 @@
+public class PrimTypes {
+    public static void main(String[] args) {
+        
+        boolean tf = true;
+        System.out.println("Boolean: " + tf);
+
+        char c = 'x';
+        System.out.println("char: " + c);
+
+        byte b = 100;
+        System.out.println("Byte: " + b);
+
+        short s = 32000;
+        System.out.println("Short: " + s);
+
+        int i = 2147483647;
+        System.out.println("Int: " + i);
+
+        long l = 2147483649l;
+        System.out.println("long: " + l);
+
+        float f = 32.37f;
+        System.out.println("Float: " + f);
+
+        double x = 65.789;
+        System.out.println("Double: " + x);
+
+        String name = "Kevin OBrien";
+        System.out.println("String: " + name);
+    }    
+}
---
+++ week3/Shapes.java
@@ -0,0 +1,40 @@
+public class Shapes {
+    public static void main(String[] args) {
+        //Exercise 1
+        System.out.println("********       *            *        *");
+        System.out.println("********      ***      *******      ***");
+        System.out.println("********     *****     ********    *****");
+        System.out.println("********    *******    *******      ***");
+        System.out.println("********   *********        *        *");
+
+        System.out.println();
+
+        // Exercise 2    
+        System.out.println("+---------------------------+");
+        System.out.println("|                           |");
+        System.out.println("|    LocalGarage            |");
+        System.out.println("|  12-OCT-2021  09:10PM     |");
+        System.out.println("|                           |");
+        System.out.println("|    Litres:      44.00     |");
+        System.out.println("|    Price/Litre:  1.15     |");
+        System.out.println("|                           |");
+        System.out.println("|    Fuel Total: 50.60      |");
+        System.out.println("|                           |");
+        System.out.println("+---------------------------+");
+
+        System.out.println();
+
+        // Exercise 3
+        System.out.println("-----------------------------------------");
+        System.out.println("|# |Team\t|P |W |L |D | Points\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|1 |Chelsea\t|7 |5 |1 |1 | 16\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|2 |Liverpool\t|7 |4 |0 |3 | 15\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|3 |Man City\t|7 |4 |1 |2 | 14\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|4 |Man Utd\t|7 |4 |1 |2 | 14\t|");
+        System.out.println("-----------------------------------------");
+    }
+}
