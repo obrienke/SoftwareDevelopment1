6c0125bac683f2b4bef00897814fa0d292c545a6
obrienke <kevin.obrien@gmit.ie>
2024-02-09 10:11:48 +0000

    exercise 4 from lab

---
+++ week15/TenLoops.java
@@ -0,0 +1,9 @@
+public class TenLoops {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 10){
+            System.out.println(i+ ". See me ten times");
+            i++;
+        }
+    }
+}
