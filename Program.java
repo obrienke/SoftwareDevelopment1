e26ae60efdb0567e6621c92a7b0ffe75c22084c4
obrienke <kevin.obrien@gmit.ie>
2024-02-26 11:30:44 +0000

    Program Template

--- Program.java
+++ Program.java
@@ -1,7 +1,16 @@
-// Program Name: 
-
+//import java.util.Scanner;
 public class Program{
-	public static void main(String[] args){
-		//input code here
-	}
+    public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
+        
+        // list variables
+
+        // inputs required
+
+        // if / else, and/or loops
+
+        // outputs
+
+        // input.close();
+    }
 }
