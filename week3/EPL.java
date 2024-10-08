67bb43613292812d40e9bc5e591661c358d2eca4
obrienke <kevin.obrien@gmit.ie>
2024-10-08 13:19:07 +0100

    week 3 lab solutions

---
+++ week3/EPL.java
@@ -0,0 +1,23 @@
+// EPL.java - exercise 8
+// Text-printing program
+
+public class EPL{
+
+  //main method begins the execution of Java Application
+  public static void main(String[] args){
+    System.out.println("-----------------------------------------");
+    System.out.println("|# |Team\t|P |W |L |D |Points\t|");
+    System.out.println("-----------------------------------------");
+    System.out.println("|1 |Chelsea\t|7 |5 |1 |1 |16\t\t|");
+    System.out.println("-----------------------------------------");
+    System.out.println("|2 |Liverpool\t|7 |4 |0 |3 |15\t\t|");
+    System.out.println("-----------------------------------------");
+    System.out.println("|3 |Man City\t|7 |4 |1 |2 |14\t\t|");
+    System.out.println("-----------------------------------------");
+    System.out.println("|4 |Man Utd\t|7 |4 |1 |2 |14\t\t|");
+    System.out.println("-----------------------------------------");
+
+
+  }//end of main method
+
+}//end of class
---
+++ week3/PetrolReceipt.java
@@ -0,0 +1,17 @@
+//exercise 1 - must have own name on receipt
+public class PetrolReceipt{
+  public static void main(String[] args){
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|     Local Garage       |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 12-OCT-2021  09:10PM   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Litres:       44.00    |" );
+    System.out.println( "| Price/litre:   1.15    |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total:    50.60   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
