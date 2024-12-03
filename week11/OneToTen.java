0eeb4597c106de04e69576a5362d9b6d82e6257f
obrienke <kevin.obrien@gmit.ie>
2024-12-03 16:16:26 +0000

    Example from class

--- week11/OneToTen.java
+++ week11/OneToTen.java
@@ -6,47 +6,51 @@ public class OneToTen{
         int number = 0;
 
         System.out.print("Enter a number from 1 to 10: ");
-        number = kb.nextInt();
+        if(kb.hasNextInt()){
+            number = kb.nextInt();
 
-        if(number > 0 && number <= 10){
-            System.out.println("Valid number entered.");
-            if(number % 2 == 0){
-                System.out.println(number + " is even");
-                System.out.print(number);
-                if(number == 2){
-                    number = number * 2;
-                }else if(number == 4){
-                    number = number * 4;
-                }else if(number == 6){
-                    number = number * 6;
-                }else if(number == 8){
-                    number = number * 8;
+            if(number > 0 && number <= 10){
+                System.out.println("Valid number entered.");
+                if(number % 2 == 0){
+                    System.out.println(number + " is even");
+                    System.out.print(number);
+                    if(number == 2){
+                        number = number * 2;
+                    }else if(number == 4){
+                        number = number * 4;
+                    }else if(number == 6){
+                        number = number * 6;
+                    }else if(number == 8){
+                        number = number * 8;
+                    }else{
+                        number = number * 10;
+                    }
                 }else{
-                    number = number * 10;
+                    System.out.println(number + " is odd"); 
+                    System.out.print(number);
+                    switch(number){
+                        case 1:
+                            number = number + 1;
+                            break;
+                        case 3:
+                            number = number + 3;
+                            break;
+                        case 5:
+                            number = number + 5;
+                            break;
+                        case 7:
+                            number = number + 7;
+                            break;
+                        default:
+                            number = number + 9;
+                    }
                 }
+                System.out.println(" is updated to " + number);
             }else{
-                System.out.println(number + " is odd"); 
-                System.out.print(number);
-                switch(number){
-                    case 1:
-                        number = number + 1;
-                        break;
-                    case 3:
-                        number = number + 3;
-                        break;
-                    case 5:
-                        number = number + 5;
-                        break;
-                    case 7:
-                        number = number + 7;
-                        break;
-                    default:
-                        number = number + 9;
-                }
+                System.out.println("Number entered was not from 1 to 10.");
             }
-            System.out.println(" is updated to " + number);
         }else{
-            System.out.println("Invalid number entered.");
+            System.out.println("Number entered is invalid.");
         }
         kb.close();
     }
