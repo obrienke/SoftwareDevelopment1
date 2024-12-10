bee07814b820eed6200733a0c4f82cc6429b396a
obrienke <kevin.obrien@gmit.ie>
2024-12-10 16:38:19 +0000

    week 12 lab

---
+++ week12/X.java
@@ -0,0 +1,13 @@
+public class X{
+  public static void main(String[] args){
+    System.out.println();
+    System.out.println(" ***   ***");
+    System.out.println("  *** ***");
+    System.out.println("   *****");
+    System.out.println("    ***");
+    System.out.println("   *****");
+    System.out.println("  *** ***");
+    System.out.println(" ***   ***");
+    System.out.println();
+  }
+}
