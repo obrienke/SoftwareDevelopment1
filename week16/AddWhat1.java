59fcb08a6e75e87124e9eb7f7e021b467a043dfd
obrienke <kevin.obrien@gmit.ie>
2025-02-17 12:49:17 +0000

    Update AddWhat1.java

--- week16/AddWhat1.java
+++ week16/AddWhat1.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class AddWhat1{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-        int x = 0;
+        	int x = 0;
 
-        System.out.print("Enter a number: ");
-        x = input.nextInt();
-
-        if(x > 20){
-            x += 2;
-        }else if(x > 10 && x <= 20){
-            x += 3;
-        }else{
-            x++;
-        }
-
-        System.out.println("Number updated to: "+x);
-		
+	        System.out.print("Enter a number: ");
+	        x = input.nextInt();
+	
+	        if(x > 20){
+	            x += 2;
+	        }else if(x > 10 && x <= 20){
+	            x += 3;
+	        }else{
+	            x++;
+	        }
+	
+	        System.out.println("Number updated to: "+x);
+			
 		input.close();	
 	}
 }
