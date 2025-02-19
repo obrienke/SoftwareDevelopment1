6da2beac6c512c0144f46cf87a92cdb7174ab720
obrienke <kevin.obrien@gmit.ie>
2025-02-19 20:18:25 +0000

    week 17 update

--- Exercise2.java
+++
@@ -1,7 +0,0 @@
-public class Exercise2{
-    public static void main(String[] args){
-        for(int i = 1; i <= 20; i++){
-            System.out.println("iteration " + i);
-        }
-    }
-}
--- FirstFor.java
+++
@@ -1,7 +0,0 @@
-public class FirstFor{
-    public static void main(String[] args){
-        for(int i = 1; i <= 5; i++){
-            System.out.println(i);
-        }
-    }
-}
