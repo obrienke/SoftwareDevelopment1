82e91ed7d56aafa9847a1e825ff42690b9f6e419
obrienke <kevin.obrien@gmit.ie>
2025-04-04 09:55:48 +0000

    sample assessment solutions

---
+++ week23/SampleEx1.java
@@ -0,0 +1,12 @@
+public class SampleEx1 {
+    public static void main(String[] args) {
+        String name = "Jim";
+        String job = "carpenter";
+        int age = 47;
+        double wages = 878.23;
+
+        System.out.println(name + " works as a " + job + ".");
+        System.out.println("He is " + age + " years old.");
+        System.out.println("He earns " + wages + " after tax");
+    }
+}
