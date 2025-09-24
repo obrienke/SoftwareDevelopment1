bc10e8dd71e3b41af11dd951bfa72d127b3d286d
obrienke <kevin.obrien@gmit.ie>
2025-09-24 20:21:45 +0000

    testing codespaces

--- week1/Welcome1.java
+++ week1/Welcome1.java
@@ -1,5 +1,7 @@
 public class Welcome1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Programming.");
+        System.out.println("Welcome to Java Programming.");
+        System.out.println("Welcome to Java Programming.");
     }
 }
