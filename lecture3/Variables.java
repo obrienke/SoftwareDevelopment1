1a0ae74dd740ecd20dda6d46e97ab60d0bf03523
obrienke <kevin.obrien@gmit.ie>
2025-10-02 10:19:04 +0000

    code from lecture 3

---
+++ lecture3/Variables.java
@@ -0,0 +1,24 @@
+public class Variables{
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;   // Floating point number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myText = "Hello";    // String
+        short myShort = 5000;      // Short
+        byte myByte = 100;         // Byte
+        long myLong = 15000000000L; // Long
+        System.out.println(myNum);
+        myNum = 10;
+        System.out.println(myNum);
+        double myDouble = 19.99d; // Double
+        System.out.println(myDouble);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+        System.out.println(myShort);
+        System.out.println(myByte); 
+        System.out.println(myLong);
+    }
+}
